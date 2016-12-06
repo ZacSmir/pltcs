07f6cc7f29a22e2b462cf71c62c7f525212f91fd
ZacSmir <Zach@smirin.us>
2016-12-06 08:53:16 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ $.ajax({
   method: "GET",
   url: "https://www.govtrack.us/api/v2/person",
   success: function(data) {    
+    var arr = []
     data.objects.forEach(function(object) {
+      arr.push(object.firstname)
       console.log(object.firstname)
      
     })
